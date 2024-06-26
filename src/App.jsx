1228445189213bd10ee7058c727fe23910a8b1fd
Mobi <chimaojimma@gmail.com>
2024-06-26 18:09:14 +0100

    add new route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import ErrorPage from "./components/errorpage"
 import Info from "./components/info"
 import {Plan} from "./components/plan"
+import AddOn from "./components/add-ons"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
          <Route path="*" element={<ErrorPage />} />
          <Route path="/" element={<Info />} />
          <Route path="plan" element={<Plan />} />
+         <Route path="add-on" element={<AddOn />} />
       </Routes>
    </BrowserRouter>
   )
