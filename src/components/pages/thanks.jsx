4147cdfa6d4551c6d93ce362bf2a738d20febbb4
Mobi <chimaojimma@gmail.com>
2024-06-29 19:59:21 +0100

    Finalise resizing

--- src/components/pages/thanks.jsx
+++ src/components/pages/thanks.jsx
@@ -10,10 +10,10 @@ function Thanks() {
          </MediaQuery>
          <div className="m-auto flex flex-col justify-center items-center gap-3 text-[rgb(2,41,90)]">
             <img src="/subscription_system/images/components/thanks/icon-thank-you.svg" width={'100px'} alt="THANKS FOR SUBSCRIBING" />
-            <h3 className="mt-4 font-bold text-3xl">
+            <h3 className="mt-4 font-bold text-2xl">
                Thank you!
             </h3>
-            <p className="text-sm w-[380px] text-center font-normal">
+            <p className="text-[12.5px] w-[380px] text-center font-normal">
                Thanks for confirming your subscription! We hope you have fun using out platform. If you ever need support, please feel free to email us at support@loremgaming.com
             </p>
          </div>
--- src/components/utils/buttons.jsx
+++ src/components/utils/buttons.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Next() {
   return (
    <button type='submit'
-   className='text-md w-fit text-white px-6 rounded-lg py-2 self-end mt-20 bg-[rgb(2,41,90)]' >
+   className='text-[12.5px] w-fit text-white px-4 rounded-md py-2 self-end mt-20 bg-[rgb(2,41,90)]' >
    Next Step
 </button>
   )
@@ -14,7 +14,7 @@ function Prev({back}) {
    return (
       <Link 
          to={back}
-         className='text-sm font-medium w-fit text-[rgb(2,41,90)] mt-20' >
+         className='text-[12.5px] font-medium w-fit text-[rgb(2,41,90)] mt-20' >
          Go Back
       </Link>
       )
@@ -25,7 +25,7 @@ function Prev({back}) {
        <Link
          replace
          to={link}
-         className='text-md w-fit text-white px-6 rounded-lg py-2 self-end mt-20 bg-[hsl(243,100%,62%)]' >
+         className='text-[12.5px] w-fit text-white px-4 rounded-lg py-2 self-end mt-20 bg-[hsl(243,100%,62%)]' >
        Confirm
     </Link>
       )
